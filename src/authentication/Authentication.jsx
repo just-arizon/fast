7afcc26608dd90710263be489e44327cbfa0cced
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2024-07-09 01:14:24 +0100

    vercel-test

--- src/authentication/Authentication.jsx
+++ src/authentication/Authentication.jsx
@@ -4,23 +4,24 @@ import Signin from './Signin';
 import Signup from './Signup';
 
 const Authentication = () => {
-  const [active, setActive] = useState("signin");
+  // const [active, setActive] = useState("signin");
 
-  const handleChange = () => {
-    setActive(active === "signin" ? "signup" : "signin");
-  };
+  // const handleChange = () => {
+  //   setActive(active === "signin" ? "signup" : "signin");
+  // };
 
   return (
-    <AnimatePresence>
-      <motion.div className="relative isolate overflow-hidden min-h-screen">
-        <img
-          src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=7000&sat=-50&exp=15&blend-mode=multiply"
-          alt=""
-          className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-        />
-        {active === "signin" ? <Signin handleChange={handleChange} /> : <Signup handleChange={handleChange} />}
-      </motion.div>
-    </AnimatePresence>
+    // <AnimatePresence>
+    //   <motion.div className="relative isolate overflow-hidden min-h-screen">
+    //     <img
+    //       src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=7000&sat=-50&exp=15&blend-mode=multiply"
+    //       alt=""
+    //       className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+    //     />
+    //     {active === "signin" ? <Signin handleChange={handleChange} /> : <Signup handleChange={handleChange} />}
+    //   </motion.div>
+    // </AnimatePresence>
+    <div>Hello</div>
   );
 };
 
