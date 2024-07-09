b4be427797f1af9b60d627ffbc6cbbd89ff85456
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2024-07-09 19:44:49 +0100

    add macbook mark-up

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,26 @@ import {BrowserRouter} from 'react-router-dom'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import '@fontsource/roboto'; // Import font
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 
 const theme = createTheme({
   typography: {
     fontFamily: 'Roboto, Arial, sans-serif',
+    h1: {
+      fontWeight: 700,
+    },
+    body1: {
+      fontWeight: 400,
+    },
+    button: {
+      fontWeight: 500,
+    },
+    // Add more typography variants as needed
   },
 });
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AnimatePresence, motion } from "framer-motion"
 import Button from '@mui/material/Button';
-
+import MacBook from '../../public/Macbook Pro Render.png'
 const links = [
     { name: 'Sign In', href: '/sign-in' },
     { name: 'Sign Up', href: '/sign-up' },
@@ -19,16 +19,16 @@ const links = [
     return (
         <AnimatePresence>
       <motion.div className="relative isolate overflow-hidden  py-24 sm:py-32">
-        {/* <img
+        <img
           src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
           alt=""
           className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-        /> */}
-        <img
+        />
+        {/* <img
           src="https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?q=80&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&crop=focalpoint&fp-y=2&w=2830&h=1500&w=2830&h=1500&q=80&blend=7000&sat=-50&exp=15&blend-mode=multiply"
           alt=""
           className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-        />
+        /> */}
         <div
           className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
           aria-hidden="true"
@@ -53,39 +53,39 @@ const links = [
             }}
           />
         </div>
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0">
-            <motion.h1 
-            initial={
-                {
-                    y: 100,
-                    opacity: 0,
-					scale: 0.9,
-                }}
-                animate={{
-                    y: 0,
-                    opacity: 1,
-                    scale: 1,
-                }}
-                // exit={{
-                //     y: -100,
-                //     opacity: 0,
-				// 	scale: 0.9,
-                // }}
-                transition={{
-                    // duration: 0.5,
-                    ease:"easeOut"
-                }}
-            className="text-4xl font-bold tracking-tight text-gray-800 sm:text-6xl">
-              Work with us
-              </motion.h1>
-            <p className="mt-6 text-lg leading-8 text-white">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
-            </p>
-          </div>
-          <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-            <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-[#fff000] sm:grid-cols-2 md:flex lg:gap-x-10">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8 flex flex-col justify-between gap-28">
+
+          <div className="mx-auto max-w-6xl max-h-64 lg:mx-0 flex justify-between">
+            <div>
+              <motion.h1
+              initial={
+                  {
+                      y: 100,
+                      opacity: 0,
+                        scale: 0.9,
+                  }}
+                  animate={{
+                      y: 0,
+                      opacity: 1,
+                      scale: 1,
+                  }}
+                  // exit={{
+                  //     y: -100,
+                  //     opacity: 0,
+                      // 	scale: 0.9,
+                  // }}
+                  transition={{
+                      // duration: 0.5,
+                      ease:"easeOut"
+                  }}
+              className="text-4xl font-bold tracking-tight text-gray-800 sm:text-6xl">
+                Work with us
+                </motion.h1>
+              <p className="mt-6 text-lg leading-8 text-white">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+                fugiat veniam occaecat fugiat aliqua.
+              </p>
+              <div className="grid grid-cols-1 gap-x-5 gap-y-6 text-base font-semibold leading-7 text-[#fff000] py-6 sm:grid-cols-2 md:flex lg:gap-x-10">
                 {links.map((link) => (
                     <a key={link.name} href={link.href}>
                       <Button variant='contained'>{link.name}<span aria-hidden="true">&rarr;</span>
@@ -93,6 +93,16 @@ const links = [
                     </a>
                 ))}
             </div>
+            </div>
+            <div>
+              <img src={MacBook} alt="image" srcset="" />
+            </div>
+          </div>
+
+
+
+          <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+        
             <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4 ">
               {stats.map((stat) => (
                 <div key={stat.name} className="flex flex-col-reverse">
